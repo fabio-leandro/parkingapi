5f4c0488ec43e65a712be0b48d7e73434125fa2b
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-08-19 16:08:28 -0300

    Documentando com swagger

--- src/main/java/com/fabio/parkingapi/controllers/ParkingController.java
+++ src/main/java/com/fabio/parkingapi/controllers/ParkingController.java
@@ -5,6 +5,8 @@ import com.fabio.parkingapi.dtos.NewParkingDto;
 import com.fabio.parkingapi.dtos.ParkingDto;
 import com.fabio.parkingapi.dtos.UpdateParkingDto;
 import com.fabio.parkingapi.services.ParkingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,46 +17,54 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/${api.version}/parkings")
+@Api(tags = "Parking Controller")
 public class ParkingController {
 
     @Autowired
     private ParkingService parkingService;
 
     @GetMapping
+    @ApiOperation(value = "Find all vehicles parked.")
     public ResponseEntity<List<ParkingDto>> findALl(){
         return ResponseEntity.ok(parkingService.findAll());
     }
 
     @PostMapping
+    @ApiOperation(value = "Create a parking and return ParkingDto.")
     public ResponseEntity<ParkingDto> saveParking(@Valid @RequestBody NewParkingDto newParkingDto){
         ParkingDto parkingDto = parkingService.saveParking(newParkingDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(parkingDto);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Find a parking by ID and return a ParkingDto")
     public ResponseEntity<ParkingDto> findById(@PathVariable Long id){
         ParkingDto parkingDto = parkingService.findById(id);
         return ResponseEntity.ok(parkingDto);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Find a parking by ID and update It.")
     public ResponseEntity<ParkingDto> updateById(@PathVariable Long id, @Valid @RequestBody UpdateParkingDto updateParkingDto){
         ParkingDto dto = parkingService.updateById(id,updateParkingDto);
         return ResponseEntity.ok(dto);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Find a parking by ID and delete It.")
     public ResponseEntity<Void> deleteById(@PathVariable Long id){
         parkingService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/parkedTime/{id}")
+    @ApiOperation("Calculate a minutes total between entrance and exit of parking.")
     public ResponseEntity<Integer> getFraction(@PathVariable Long id){
         return ResponseEntity.ok(parkingService.getFraction(id));
     }
 
     @PutMapping("/generateBill/{id}")
+    @ApiOperation(value = "Find a parking by ID and get a bill of a parking. It return a ParkingDto with bill.")
     public ResponseEntity<ParkingDto> generateBill(@RequestParam(value = "price") Double price,
                                               @RequestParam(value = "adicional") Double adicional,
                                               @PathVariable Long id){
--- src/main/java/com/fabio/parkingapi/controllers/PriceTableController.java
+++ src/main/java/com/fabio/parkingapi/controllers/PriceTableController.java
@@ -4,6 +4,8 @@ import com.fabio.parkingapi.dtos.NewPriceTableDto;
 import com.fabio.parkingapi.dtos.PriceTableDto;
 import com.fabio.parkingapi.dtos.UpdatePriceTableDto;
 import com.fabio.parkingapi.services.PriceTableService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/${api.version}/prices")
+@Api(tags = "Price Table Controller")
 public class PriceTableController {
 
     @Autowired
@@ -25,21 +28,25 @@ public class PriceTableController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Find all prices.")
     public ResponseEntity<PriceTableDto> save(@Valid @RequestBody NewPriceTableDto dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(priceTableService.save(dto));
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Find price by ID.")
     public ResponseEntity<PriceTableDto> findById(@PathVariable Long id){
         return ResponseEntity.ok(priceTableService.findById(id));
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Update a price.")
     public ResponseEntity<PriceTableDto> updateById(@PathVariable Long id, @RequestBody UpdatePriceTableDto dto){
         return ResponseEntity.ok(priceTableService.updateById(id,dto));
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete a price.")
     public ResponseEntity<Void> deleteById(@PathVariable Long id){
         priceTableService.deleteById(id);
         return ResponseEntity.noContent().build();
